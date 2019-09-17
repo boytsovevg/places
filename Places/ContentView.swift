17b7a427126aff460b412aabbdb710326e3f24d4
Eugene <evgeniy.boytsov@gmail.com>
2019-09-17 16:30:55 +0300

    Initial Commit

---
+++ Places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Places
+//
+//  Created by Evgeniy Boytsov on 17.09.2019.
+//  Copyright © 2019 boytsovevg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
